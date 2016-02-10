62cb7f317d4b6efe77de4349d2081c4c45da1521
Lin Clark <lclark@mozilla.com>
2016-02-10 09:33:31 -0800

    add test data for ConsoleGeneric

---
+++ test/data/console-output/ConsoleGeneric.js
@@ -0,0 +1,23 @@
+export default {
+  "from": "server1.conn4.child1/consoleActor2",
+  "type": "consoleAPICall",
+  "message": {
+    "arguments": [
+      "foobar",
+      "test"
+    ],
+    "columnNumber": 1,
+    "counter": null,
+    "filename": "file:///Users/lclark/Documents/test.html",
+    "functionName": "",
+    "groupName": "",
+    "level": "log",
+    "lineNumber": 1,
+    "private": false,
+    "styles": [],
+    "timeStamp": 1455064271115,
+    "timer": null,
+    "workerType": "none",
+    "category": "webdev"
+  }
+}
