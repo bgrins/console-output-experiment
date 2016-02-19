372164559168b8341a19708038a6f276062cd21d
Brian Grinstead <briangrinstead@gmail.com>
2016-02-19 12:56:59 -0800

    Update toolbar to stub out filter and search features

--- js/components/ConsoleToolbar.js
+++ js/components/ConsoleToolbar.js
@@ -5,11 +5,33 @@ import { connect } from 'react-redux';
 class ConsoleToolbar extends Component {
   render() {
     let clear = () => {
+      console.log("CLEAR");
       this.props.dispatch(messageClear());
     };
+    let severityFilter = () => {
+      // @TODO Add severity filtering
+    };
+    let search = () => {
+      // @TODO Implement search feature
+    };
+
 
     return (
-      <button onClick={clear}>Clear</button>
+      <div className="console-toolbar">
+        <div className="console-toolbar-start">
+          <button onClick={clear}>Clear</button>
+          <button onClick={severityFilter}>All</button>
+          <button onClick={severityFilter}>Errors</button>
+          <button onClick={severityFilter}>Warnings</button>
+          <button onClick={severityFilter}>Info</button>
+          <button onClick={severityFilter}>Log</button>
+        </div>
+        <div className="console-toolbar-end">
+          <input onInput={search} placeholder="Search (TODO)" /> &nbsp;
+          <a href="https://github.com/bgrins/console-output-experiment/">repo</a>
+        </div>
+      </div>
+
     );
   }
 }
